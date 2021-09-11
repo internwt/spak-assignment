91622e4cb217986825e0cb2bd091dce0c83c8bc8
FIRST_NAME LAST_NAME <loveneet@biz4group.com>
2021-09-11 19:50:50 +0530

    end-point edded

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3001
 
-
+app.get('/', (req, res) => {
+    res.send("hello wrold")
+})
 app.listen(port, () => {
     console.log(`port in running ${port}`)
 });
